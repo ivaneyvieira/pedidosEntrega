ab01d5b640d4bd86b19635a0c115af185606e412
ivaneyvieira <ivaneyvieira@gmail.com>
2020-05-18 08:53:24 -0300

    Conserta o título da tela de login

--- src/main/kotlin/br/com/astrosoft/framework/model/RegistryUserInfo.kt
+++ src/main/kotlin/br/com/astrosoft/framework/model/RegistryUserInfo.kt
@@ -5,7 +5,7 @@ import br.com.astrosoft.framework.util.SystemUtils
 object RegistryUserInfo {
   var loginInfoProvider: LoginInfoProvider? = null
   val version = SystemUtils.readFile("/versao.txt") ?: "1.0"
-  const val appName = "Abastecimento de loja"
+  const val appName = "Pedido para Entrega"
   const val commpany = "Engecopi"
   val usuario
     get() = loginInfoProvider?.loginInfo?.usuario

4e7f1b59108553e112cd6fc87cb13ff56e9a2b2b
ivaneyvieira <ivaneyvieira@gmail.com>
2020-09-11 23:47:34 -0300

    Calculo frete + total parcial no dialogo de detalhe

--- src/main/kotlin/br/com/astrosoft/pedido/model/beans/EntregadorNotas.kt
+++ src/main/kotlin/br/com/astrosoft/pedido/model/beans/EntregadorNotas.kt
@@ -1,8 +1,9 @@
 package br.com.astrosoft.pedido.model.beans
 
+import br.com.astrosoft.framework.util.format
 import java.time.LocalDate
 
-data class EntregadorNotas(
+class EntregadorNotas(
   val cargano: Int,
   val funcaoName: String,
   val nome: String,
@@ -15,9 +16,9 @@ data class EntregadorNotas(
   val prdno: String,
   val grade: String,
   val descricao: String,
-  val pisoCxs: Int,
-  val pisoPeso: Double,
-  val valor: Double,
+  val pisoCxs: Int?,
+  val pisoPeso: Double?,
+  val valor: Double?,
   val valorNota: Double,
   val valorFrete: Double
                           ): Any() {
@@ -33,6 +34,8 @@ data class EntregadorNotas(
     get() = if(funcaoName == "") null else date
   val carganoCol
     get() = if(funcaoName == "") null else cargano
+  val prdnoCol
+    get() = if(funcaoName == "") null else prdno
   
   fun groupByNota() = EntregadorNotasGroup(loja, nota, numPedido, datePedido, valorNota, valorFrete)
 }
@@ -43,3 +46,81 @@ data class EntregadorNotasGroup(val loja: Int,
                                 val datePedido: LocalDate?,
                                 val valorNota: Double,
                                 val valorFrete: Double)
+
+fun List<EntregadorNotas>.groupByNota(): List<EntregadorNotas> {
+  val groupBy = this.groupBy {entregadorNota ->
+    entregadorNota.groupByNota()
+  }.entries
+  val groupValor = groupBy.map {entry ->
+    val valorNota = entry.key.valorNota
+    val valorFrete = entry.key.valorFrete
+    
+    EntregadorNotas(cargano = 0,
+                    funcaoName = "",
+                    nome = "",
+                    date = null,
+                    empno = 0,
+                    loja = entry.key.loja,
+                    nota = entry.key.nota,
+                    numPedido = entry.key.numPedido,
+                    datePedido = entry.key.datePedido,
+                    prdno = "ZZZZZZ",
+                    grade = "",
+                    descricao = "Total parcial",
+                    pisoCxs = entry.value.sumBy {it.pisoCxs ?: 0},
+                    pisoPeso = entry.value.sumByDouble {it.pisoPeso ?: 0.00},
+                    valor = valorNota,
+                    valorNota = valorNota,
+                    valorFrete = valorFrete
+                   )
+  }
+  val groupFrete = groupBy.map {entry ->
+    val valorFrete = entry.key.valorFrete
+    EntregadorNotas(cargano = 0,
+                    funcaoName = "",
+                    nome = "",
+                    date = null,
+                    empno = 0,
+                    loja = entry.key.loja,
+                    nota = entry.key.nota,
+                    numPedido = entry.key.numPedido,
+                    datePedido = entry.key.datePedido,
+                    prdno = "MMMMMM",
+                    grade = "",
+                    descricao = "Frete",
+                    pisoCxs = null,
+                    pisoPeso = null,
+                    valor = valorFrete,
+                    valorNota = 0.00,
+                    valorFrete = 0.00
+                   )
+  }
+  val group = groupFrete + groupValor
+  val totalGeral = EntregadorNotas(cargano = 0,
+                                   funcaoName = "",
+                                   nome = "",
+                                   date = null,
+                                   empno = 0,
+                                   loja = 999,
+                                   nota = "",
+                                   numPedido = 0,
+                                   datePedido = null,
+                                   prdno = "",
+                                   grade = "",
+                                   descricao = "Total geral + Frete: ${
+                                     group.sumByDouble {it.valorFrete}
+                                       .format()
+                                   }",
+                                   pisoCxs = this.sumBy {it.pisoCxs ?: 0},
+                                   pisoPeso = this.sumByDouble {it.pisoPeso ?: 0.00},
+                                   valor = group.sumByDouble {it.valorNota},
+                                   valorNota = group.sumByDouble {it.valorNota},
+                                   valorFrete = group.sumByDouble {it.valorFrete}
+                                  )
+  val joinList = group + this + totalGeral
+  return joinList.sortedWith(compareBy({it.loja},
+                                       {it.nota},
+                                       {it.numPedido},
+                                       {it.datePedido},
+                                       {it.prdno}))
+}

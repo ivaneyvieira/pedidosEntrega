f6312a996f2878a3e816f80e09862cc0ee845965
ivaneyvieira <ivaneyvieira@gmail.com>
2020-10-09 10:54:09 -0300

    Pedido com desconto

--- src/main/resources/sqlSaci/produtoPedido.sql
+++ src/main/resources/sqlSaci/produtoPedido.sql
@@ -7,10 +7,12 @@ SELECT CAST(LPAD(E.prdno * 1, 6, '0') AS CHAR) AS codigo,
        IFNULL(B.barcode, P.barcode)            AS barcode,
        ROUND(E.qtty / 1000)                    AS qtd,
        P.weight                                AS peso,
-       E.price / 100                           AS vlUnit,
+       IFNULL(X.price, E.price) / 100          AS vlUnit,
        ROUND(E.qtty / 1000) * (E.price / 100)  AS vlTotal,
        IFNULL(L.localizacao, '')               AS localizacao
 FROM sqldados.eoprd          AS E
+  INNER JOIN sqldados.eord   AS EO
+	       USING (storeno, ordno)
   INNER JOIN sqldados.prd    AS P
 	       ON P.no = E.prdno
   LEFT JOIN  sqldados.prdbar AS B
@@ -20,8 +22,10 @@ FROM sqldados.eoprd          AS E
   LEFT JOIN  sqldados.eoprdf AS O
 	       ON (O.storeno = E.storeno AND O.ordno = E.ordno AND O.prdno = E.prdno AND
 		   O.grade = E.grade)
+  LEFT JOIN  sqldados.xaprd  AS X
+	       ON X.storeno = EO.storeno AND X.nfno = EO.nfno AND X.nfse = EO.nfse AND
+		  X.prdno = E.prdno AND X.grade = E.grade
 WHERE E.storeno = :storeno
   AND E.ordno = :ordno
-  AND (((@TIPO = 'R') AND (O.bits & POW(2, 1))) OR
-       ((@TIPO = 'E') AND (NOT O.bits & POW(2, 1))))
+  AND (((@TIPO = 'R') AND (O.bits & POW(2, 1))) OR ((@TIPO = 'E') AND (NOT O.bits & POW(2, 1))))
 GROUP BY E.storeno, E.ordno, E.prdno, E.grade

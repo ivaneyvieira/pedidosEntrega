e20fcb917666483b09f3053851b0c043e550e168
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-09-30 08:56:59 -0300

    Suplica

--- src/main/kotlin/br/com/astrosoft/pedido/viewmodel/entrega/PedidoEntregaImpressoSepararViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/pedido/viewmodel/entrega/PedidoEntregaImpressoSepararViewModel.kt
@@ -6,37 +6,39 @@ import br.com.astrosoft.pedido.model.beans.ETipoPedido.ENTREGA
 import br.com.astrosoft.pedido.model.beans.FiltroPedido
 import br.com.astrosoft.pedido.model.beans.Pedido
 
-class PedidoEntregaImpressoComNotaViewModel(val viewModel: PedidoEntregaViewModel) {
+class PedidoEntregaImpressoSemNotaViewModel(val viewModel: PedidoEntregaViewModel) {
   private val subView
-    get() = viewModel.view.tabEntregaImpressoComNota
+    get() = viewModel.view.tabEntregaImpressoSemNota
 
-  private fun listPedidosEntregaImpressoComNota(): List<Pedido> {
-    val numPedido = subView.pedidoImpressoComNota
-    return Pedido.listaPedidoImpressoComNota(FiltroPedido(tipo = ENTREGA,
+  private fun listPedidosEntregaImpressoSemNota(): List<Pedido> {
+    val pesquisa = subView.pedidoPesquisa
+    return Pedido.listaPedidoImpressoSemNota(FiltroPedido(tipo = ENTREGA,
+                                                          pesquisa = pesquisa,
                                                           ecommerce = false,
                                                           dataInicial = null,
-                                                          dataFinal = null)).filter { pedido ->
-      pedido.pedido == numPedido || numPedido == 0
-    }
+                                                          dataFinal = null))
   }
 
-  fun updateGridImpressoComNota() {
-    subView.updateGrid(listPedidosEntregaImpressoComNota())
+  fun updateGridImpressoSemNota() {
+    subView.updateGrid(listPedidosEntregaImpressoSemNota())
   }
 
-  fun desmarcaComNota() = exec(viewModel.view) {
+  fun desmarcaSemNota() = exec(viewModel.view) {
     val pedidos = subView.itensSelecionado().ifEmpty { fail("Não há pedido selecionado") }
     pedidos.forEach { pedido ->
       pedido.desmarcaImpresso()
     }
 
-    updateGridImpressoComNota()
+    updateGridImpressoSemNota()
+  }
+
+  fun imprimirPedidos(itensSelecionado: List<Pedido>) {
+    viewModel.tabEntregaImprimirViewModel.imprimirPedidos(itensSelecionado)
   }
 }
 
-interface IPedidoEntregaImpressoComNota {
+interface IPedidoEntregaImpressoSemNota {
   fun updateGrid(itens: List<Pedido>)
   fun itensSelecionado(): List<Pedido>
-
-  val pedidoImpressoComNota: Int
+  val pedidoPesquisa: String
 }

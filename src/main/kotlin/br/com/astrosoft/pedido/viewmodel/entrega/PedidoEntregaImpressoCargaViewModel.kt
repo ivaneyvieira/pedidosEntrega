8f84702d420439a34cc5aa7f8d965b361ac91343
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-10-07 11:23:47 -0300

    Adição de novas zonas Leste3 e Norte3

--- src/main/kotlin/br/com/astrosoft/pedido/viewmodel/entrega/PedidoEntregaImpressoCargaViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/pedido/viewmodel/entrega/PedidoEntregaImpressoCargaViewModel.kt
@@ -71,8 +71,10 @@ interface IPedidoEntregaImpressoCarga {
 enum class EZonaCarga(val codigo: Char, val descricao: String) {
   Leste1('A', "Leste 1"),
   Leste2('B', "Leste 2"),
+  Leste3('I', "Leste 3"),
   Norte1('C', "Norte 1"),
   Norte2('D', "Norte 2"),
+  Norte3('J', "Norte 3"),
   Sul1('E', "Sul 1"),
   Sul2('F', "Sul 2"),
   Sul3('G', "Sul 3"),

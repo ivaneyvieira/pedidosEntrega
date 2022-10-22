98a4feb4e197f499b3e22e9b079ce67fe46e4ba2
Ivaney Sales <ivaneyvieira@gmail.com>
2022-10-22 12:18:30 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/pedido/viewmodel/entrega/RelatorioText.kt
+++ src/main/kotlin/br/com/astrosoft/pedido/viewmodel/entrega/RelatorioText.kt
@@ -4,6 +4,7 @@ import br.com.astrosoft.framework.util.format
 import br.com.astrosoft.framework.util.lpad
 import br.com.astrosoft.framework.viewmodel.PrintText
 import br.com.astrosoft.pedido.model.beans.Relatorio
+import br.com.astrosoft.pedido.model.beans.Rota
 import java.time.LocalDate
 import java.time.LocalTime
 
@@ -20,14 +21,15 @@ class RelatorioText : PrintText<Relatorio>() {
     val dataHora = "${LocalDate.now().format()}-${LocalTime.now().format()}"
     return listOf(""""
     
-DOCUMENTO NAO FISCAL                               $dataHora
+DOCUMENTO NAO FISCAL                            $dataHora
  
     """.trimIndent().negrito())
   }
 
   override fun titleLines(bean: Relatorio): List<String> {
+    val rota = "ROTA: ${bean.rota}".lpad(21, " ")
     return listOf(
-      "ENGECOPI ${bean.loja}       ROMANEIO DE SEPARACAO ROTA: ${bean.rota}".negrito(),
+      "ENGECOPI ${bean.loja}       ROMANEIO DE SEPARACAO $rota".negrito(),
       "PEDIDO DE ENTREGA: ${bean.pedido} ${bean.dataPedido} ${bean.notaFiscal} ${bean.dataHoraNota}".negrito(),
       "VENDEDOR: ${bean.vendedor}".negrito(),
       "CLIENTE: ${bean.cliente}".negrito(),

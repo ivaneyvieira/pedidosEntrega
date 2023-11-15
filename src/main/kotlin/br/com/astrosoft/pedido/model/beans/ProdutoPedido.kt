47e13eb443136e2f68b4ca74d37534d5c5235bdf
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-11-15 09:38:03 -0300

    Alterçao no relatorio para destacar os produtos SUBSTIFC

--- src/main/kotlin/br/com/astrosoft/pedido/model/beans/ProdutoPedido.kt
+++ src/main/kotlin/br/com/astrosoft/pedido/model/beans/ProdutoPedido.kt
@@ -2,16 +2,19 @@ package br.com.astrosoft.pedido.model.beans
 
 import br.com.astrosoft.framework.util.format
 
-data class ProdutoPedido(val codigo: String,
-                         val descricao: String,
-                         val grade: String,
-                         val refFab: String,
-                         val barcode: String,
-                         val qtd: Int,
-                         val peso: Double,
-                         val vlUnit: Double,
-                         val vlTotal: Double,
-                         val localizacao: String) {
+data class ProdutoPedido(
+  val codigo: String,
+  val descricao: String,
+  val grade: String,
+  val refFab: String,
+  val barcode: String,
+  val qtd: Int,
+  val peso: Double,
+  val vlUnit: Double,
+  val vlTotal: Double,
+  val localizacao: String,
+  val rotulo: String,
+) {
   val descricaoReport
-    get() = "$descricao\n| ${peso.format()}   | $localizacao   | $refFab "
+    get() = "$descricao\n| ${peso.format()}   | $localizacao   | $refFab  | ${if (rotulo == "SUBSTIFC") " | SUBSTIFC" else ""}"
 }
--- src/main/resources/sqlSaci/produtoPedido.sql
+++ src/main/resources/sqlSaci/produtoPedido.sql
@@ -9,22 +9,25 @@ SELECT CAST(LPAD(E.prdno * 1, 6, '0') AS CHAR) AS codigo,
        P.weight                                AS peso,
        IFNULL(X.price, E.price) / 100          AS vlUnit,
        ROUND(E.qtty / 1000) * (E.price / 100)  AS vlTotal,
-       IFNULL(L.localizacao, '')               AS localizacao
-FROM sqldados.eoprd          AS E
-  INNER JOIN sqldados.eord   AS EO
-	       USING (storeno, ordno)
-  INNER JOIN sqldados.prd    AS P
-	       ON P.no = E.prdno
-  LEFT JOIN  sqldados.prdbar AS B
-	       USING (prdno, grade)
-  LEFT JOIN  sqldados.prdloc AS L
-	       ON L.prdno = E.prdno AND L.grade = E.grade AND L.storeno = 4
-  LEFT JOIN  sqldados.eoprdf AS O
-	       ON (O.storeno = E.storeno AND O.ordno = E.ordno AND O.prdno = E.prdno AND
-		   O.grade = E.grade)
-  LEFT JOIN  sqldados.xaprd  AS X
-	       ON X.storeno = EO.storeno AND X.nfno = EO.nfno AND X.nfse = EO.nfse AND
-		  X.prdno = E.prdno AND X.grade = E.grade
+       IFNULL(L.localizacao, '')               AS localizacao,
+       IFNULL(A.form_label, '')                AS rotulo
+FROM sqldados.eoprd AS E
+       INNER JOIN sqldados.eord AS EO
+                  USING (storeno, ordno)
+       INNER JOIN sqldados.prd AS P
+                  ON P.no = E.prdno
+       LEFT JOIN sqldados.prdalq AS A
+                 ON A.prdno = P.no
+       LEFT JOIN sqldados.prdbar AS B
+                 USING (prdno, grade)
+       LEFT JOIN sqldados.prdloc AS L
+                 ON L.prdno = E.prdno AND L.grade = E.grade AND L.storeno = 4
+       LEFT JOIN sqldados.eoprdf AS O
+                 ON (O.storeno = E.storeno AND O.ordno = E.ordno AND O.prdno = E.prdno AND
+                     O.grade = E.grade)
+       LEFT JOIN sqldados.xaprd AS X
+                 ON X.storeno = EO.storeno AND X.nfno = EO.nfno AND X.nfse = EO.nfse AND
+                    X.prdno = E.prdno AND X.grade = E.grade
 WHERE E.storeno = :storeno
   AND E.ordno = :ordno
   AND ((@TIPO = 'R') AND (O.bits & POW(2, 1)) OR (@TIPO = 'E') AND (NOT O.bits & POW(2, 1)) OR
